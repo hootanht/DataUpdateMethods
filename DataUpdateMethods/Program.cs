d860d56450e3e5d93438a1e1dbd3aa2470bf5246
Hootan HT <hootanhemmati@outlook.com>
2023-04-25 14:58:15 +0330

    Add project files.

---
+++ DataUpdateMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using DataUpdateMethods.Models;
+
+namespace DataUpdateMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new WideWorldImportersContext();
+            var customer = context.Customers.Find(1);
+            Console.WriteLine(customer?.CustomerName);
+            Console.ReadKey();
+        }
+    }
+}
