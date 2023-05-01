81629bc7229f601dab46549289d21604e0216af6
Hootan HT <hootanhemmati@outlook.com>
2023-05-01 19:27:31 +0330

    Add EfCoreUpdateUpdateRangeAsync.

--- DataUpdateMethods/Program.cs
+++ DataUpdateMethods/Program.cs
@@ -1,15 +1,33 @@
-﻿using DataUpdateMethods.Models;
+﻿using System;
+using System.Security.Cryptography;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using DataUpdateMethods.Models;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace DataUpdateMethods
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var context = new WideWorldImportersContext();
-            var customer = context.Customers.Find(1);
-            Console.WriteLine(customer?.CustomerName);
+            var summary = BenchmarkRunner.Run<UpDateData>();
             Console.ReadKey();
+        }
+    }
+
+    public class UpDateData
+    {
+        [Benchmark]
+        public async Task EfCoreUpdateUpdateRangeAsync()
+        {
+            var context = new WideWorldImportersContext();
+            var invoices = await context.Invoices.ToListAsync();
+            invoices.ForEach(i => i.DeliveryInstructions = "Test");
+            context.Invoices.UpdateRange(invoices);
         }
     }
 }
