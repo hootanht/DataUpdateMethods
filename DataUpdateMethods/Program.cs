887b757dd77a7b45ecb534fd9fad26ae52d1123b
Hootan HT <hootanhemmati@outlook.com>
2023-05-05 13:35:32 +0330

    All methods are ready for test.

--- DataUpdateMethods/Program.cs
+++ DataUpdateMethods/Program.cs
@@ -13,12 +13,9 @@ namespace DataUpdateMethods
 {
     public class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             var summary = BenchmarkRunner.Run<UpDateData>();
-
-            //var p = new UpDateData();
-            //await p.EfCoreBlukUpdateAsync();
             Console.ReadKey();
         }
     }
@@ -26,54 +23,54 @@ namespace DataUpdateMethods
     [RPlotExporter]
     public class UpDateData
     {
-        //[Benchmark]
-        //public async Task EfCoreUpdateRangeAsync()
-        //{
-        //    var context = new WideWorldImportersContext();
-        //    var people = await context.People.ToListAsync();
-        //    people.ForEach(i => i.LogonName = "Test");
-        //    context.People.UpdateRange(people);
-        //    await context.SaveChangesAsync();
-        //}
-
-        //[Benchmark]
-        //public async Task EfCoreExecuteUpdateAsync()
-        //{
-        //    var context = new WideWorldImportersContext();
-        //    await context.People.ExecuteUpdateAsync(p => p.SetProperty(pr => pr.LogonName, "Test"));
-        //    await context.SaveChangesAsync();
-        //}
-
-        //[Benchmark]
-        //public async Task EfCoreBatchUpdateAsync()
-        //{
-        //    var context = new WideWorldImportersContext();
-        //    await context.Set<Person>()
-        //        .BatchUpdateAsync(new Person()
-        //        {
-        //            LogonName = "Test"
-        //        }, new List<string>()
-        //        {
-        //            nameof(Person.LogonName)
-        //        }, CancellationToken.None);
-        //}
-
-        //[Benchmark]
-        //public void EfCoreBatchUpdate()
-        //{
-        //    var context = new WideWorldImportersContext();
-        //    context.Set<Person>()
-        //       .BatchUpdate(new Person()
-        //       {
-        //           LogonName = "Test"
-        //       }, new List<string>()
-        //       {
-        //            nameof(Person.LogonName)
-        //       });
-        //}
+        [Benchmark]
+        public async Task EfCoreUpdateRangeAsync()
+        {
+            var context = new WideWorldImportersContext();
+            var people = await context.People.ToListAsync();
+            people.ForEach(i => i.LogonName = "Test");
+            context.People.UpdateRange(people);
+            await context.SaveChangesAsync();
+        }
 
         [Benchmark]
-        public async Task EfCoreBlukUpdateAsync()
+        public async Task EfCoreExecuteUpdateAsync()
+        {
+            var context = new WideWorldImportersContext();
+            await context.People.ExecuteUpdateAsync(p => p.SetProperty(pr => pr.LogonName, "Test"));
+            await context.SaveChangesAsync();
+        }
+
+        [Benchmark]
+        public async Task EfCoreBatchUpdateAsync()
+        {
+            var context = new WideWorldImportersContext();
+            await context.Set<Person>()
+                .BatchUpdateAsync(new Person()
+                {
+                    LogonName = "Test"
+                }, new List<string>()
+                {
+                    nameof(Person.LogonName)
+                }, CancellationToken.None);
+        }
+
+        [Benchmark]
+        public void EfCoreBatchUpdate()
+        {
+            var context = new WideWorldImportersContext();
+            context.Set<Person>()
+               .BatchUpdate(new Person()
+               {
+                   LogonName = "Test"
+               }, new List<string>()
+               {
+                    nameof(Person.LogonName)
+               });
+        }
+
+        [Benchmark]
+        public async Task EfCoreBulkUpdateAsync()
         {
             var context = new WideWorldImportersContext();
             var people = context.People.ToList();
